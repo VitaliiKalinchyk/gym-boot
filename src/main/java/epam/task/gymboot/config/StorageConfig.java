972573224a2eff888a06d07e172b641668f4b48a
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-05 11:14:17 +0300

    added StorageConfig

---
+++ src/main/java/epam/task/gymboot/config/StorageConfig.java
@@ -0,0 +1,40 @@
+package epam.task.gymboot.config;
+
+import epam.task.gymboot.entity.Trainee;
+import epam.task.gymboot.entity.Trainer;
+import epam.task.gymboot.entity.Training;
+import epam.task.gymboot.storage.Storage;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class StorageConfig {
+
+    private final Storage storage;
+
+    @Bean
+    @DependsOn("storageInitializerImpl")
+    public Map<Integer, Trainee> trainees() {
+        return storage.getTrainees();
+    }
+
+    @Bean
+    @DependsOn("storageInitializerImpl")
+    public Map<Integer, Trainer> trainers() {
+        return storage.getTrainers();
+    }
+
+
+    @Bean
+    @DependsOn("storageInitializerImpl")
+    public Map<Integer, Training> trainings() {
+        return storage.getTrainings();
+    }
+}
