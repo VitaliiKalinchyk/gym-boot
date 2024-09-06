2bb3ab51b2628fdc3b765a7bf242e3522a2d562a
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-06 12:16:54 +0300

    small fix

--- src/main/java/epam/task/gymboot/config/StorageConfig.java
+++ src/main/java/epam/task/gymboot/config/StorageConfig.java
@@ -8,12 +8,12 @@ import epam.task.gymboot.storage.Storage;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
-@Repository
+@Configuration
 @RequiredArgsConstructor
 public class StorageConfig {
 
