3e6d0b53d3ac7a5b592aaeb393edd698ac5b2599
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-08 12:29:00 +0300

    Added JaCoCo for checking test coverage

--- src/test/java/epam/task/gymboot/storage/initializer/StorageInitializerTest.java
+++ src/test/java/epam/task/gymboot/storage/initializer/StorageInitializerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import org.mockito.junit.jupiter.MockitoExtension;
