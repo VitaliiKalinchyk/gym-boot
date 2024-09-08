5ede10107f5cdf614f301eaaa6d22175f92d313e
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-08 11:15:10 +0300

    Using yaml for StorageInitializerTest

--- src/test/java/epam/task/gymboot/storage/initializer/StorageInitializerTest.java
+++ src/test/java/epam/task/gymboot/storage/initializer/StorageInitializerTest.java
@@ -15,7 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.Resource;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -38,50 +37,14 @@ public class StorageInitializerTest {
 
     @Test
     public void testInitializeStorage() throws IOException {
-        when(resource.getInputStream()).thenReturn(getYamlData());
+        InputStream yamlInputStream = getClass().getClassLoader().getResourceAsStream("data/initialData.yml");
+        when(resource.getInputStream()).thenReturn(yamlInputStream);
 
         storageInitializer.initializeStorage();
 
         verify(storage).setTrainees(argThat(map -> map.size() == 1 && map.containsKey(1)));
         verify(storage).setTrainers(argThat(map -> map.size() == 1 && map.containsKey(1)));
         verify(storage).setTrainings(argThat(map -> map.size() == 1 && map.containsKey(1)));
-    }
-
-    private static InputStream getYamlData() {
-        String yamlData = """
-                ---
-                trainees:
-                - traineeId: 1
-                  user:
-                    userId: 1
-                    firstName: Karl
-                    lastName: Gunner
-                    username: Karl.Gunner
-                    password: A1s12DytRd
-                    active: TRUE
-                  birthday: 1999-10-01
-                  address: Springfield, Valentine st., 4
-                trainers:
-                - trainerId: 1
-                  user:
-                    userId: 2
-                    firstName: Karl
-                    lastName: Gunner
-                    username: Karl.Gunner1
-                    password: JJFzh6qC9y
-                    active: TRUE
-                  trainingType: YOGA
-                trainings:
-                - trainingId: 1
-                  traineeId: 1
-                  trainerId: 2
-                  name: first training
-                  trainingType: ZUMBA
-                  date: 2024-08-17
-                  durationMinutes: 60
-                """;
-
-        return new ByteArrayInputStream(yamlData.getBytes());
     }
 
     @Test
